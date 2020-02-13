c825fda9cfd81ff523bcc3e7cdffdaf10e4b90f8
Wei Zhang <zhangwei1979@outlook.com>
2020-02-13 19:44:52 +0800

    Add data feedItems and method getFeedItems

--- screen/funcsstatic/js/ckeditor.vue.js
+++ screen/funcsstatic/js/ckeditor.vue.js
@@ -1,17 +1,52 @@
 
 Vue.component('ckeditor', {
     props: {id:{type:String}, content:{type:String}, name:{type:String, required:true}},
-    data: function() { return {ClassicEditor:ClassicEditor}},
+    data: function() { return {
+      ClassicEditor:ClassicEditor,
+      feedItems : [
+        { id: '@swarley', userId: '1', name: 'Barney Stinson', link: 'https://www.imdb.com/title/tt0460649/characters/nm0000439' },
+        { id: '@lilypad', userId: '2', name: 'Lily Aldrin', link: 'https://www.imdb.com/title/tt0460649/characters/nm0004989' },
+        { id: '@marshmallow', userId: '3', name: 'Marshall Eriksen', link: 'https://www.imdb.com/title/tt0460649/characters/nm0781981' },
+        { id: '@rsparkles', userId: '4', name: 'Robin Scherbatsky', link: 'https://www.imdb.com/title/tt0460649/characters/nm1130627' },
+        { id: '@tdog', userId: '5', name: 'Ted Mosby', link: 'https://www.imdb.com/title/tt0460649/characters/nm1102140' }
+      ]
+    }},
     template: '<div><textarea :id="id">{{content}}</textarea><input type="hidden" :name="name" :value="content"/></div>',
+    methods:{
+      getFeedItems:function( queryText ) {
+          return new Promise( resolve => {
+              setTimeout( () => {
+                  const itemsToDisplay = this.feedItems
+                      // Filter out the full list of all items to only those matching the query text.
+                      .filter( isItemMatching )
+                      // Return 10 items max - needed for generic queries when the list may contain hundreds of elements.
+                      .slice( 0, 10 );
+                  resolve( itemsToDisplay );
+              }, 100 );
+          } );
+
+          // Filtering function - it uses the `name` and `username` properties of an item to find a match.
+          function isItemMatching( item ) {
+              // Make the search case-insensitive.
+              const searchString = queryText.toLowerCase();
+
+              // Include an item in the search results if the name or username includes the current user input.
+              return (
+                  item.name.toLowerCase().includes( searchString ) ||
+                  item.id.toLowerCase().includes( searchString )
+              );
+          }
+      }
+    },
     mounted: function() {
       let editor;
       var jqEl = $(this.$el);
       ClassicEditor.create( this.$el.querySelector('textarea'), {
         mention: {
-          feeds: [
-              {
+          feeds: [{
                 marker: '@',
-                feed: [ '@Barney', '@Lily', '@Marshall', '@Robin', '@Ted' ],
+                //feed: [ '@Barney', '@Lily', '@Marshall', '@Robin', '@Ted' ],
+                feed: this.getFeedItems,
                 minimumCharacters: 0
               }
           ]
